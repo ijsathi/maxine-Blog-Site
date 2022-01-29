083ba26a002dd3166303eba8e5470ac601c6d682
ijsathi <ijsathi2001rs@gmail.com>
2022-01-29 14:27:40 +0600

    Extra part

---
+++ src/components/ExtraPart/ExtraPart.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
+import './ExtraPart.css';
+const ExtraPart = () => {
+    return (
+        <div className='pt-5 mt-5'>
+            <Container>
+                <Row>
+                    <Col className="pt-5" xs={6}>
+                        <div className="extra-text">
+                            <h1 className='fw-bold'>Join our newsletter 🎉</h1>
+                            <p style={{color:"#848b96"}}>Read and share new perspectives on just about any topic. Everyone’s welcome.</p>
+                            <p className='pb-2'>
+                                <span className='e-t'>01</span>
+                                <span style={{color:"#384252"}} className='fw-bold ms-2'>Get more discount</span>
+                            </p>
+                            <p>
+                                <span className='e-t2'>02</span>
+                                <span style={{color:"#384252"}} className='fw-bold ms-2'>Get premium magazines</span>
+                            </p>
+                        </div>
+                    </Col>
+                    <Col xs={6}>
+                        <div className="extra-img w-100">
+                            <img className="h-100 w-100" src="https://chisnghiax.com/ncmaz/static/media/SVG-subcribe2.efb832b2.png" alt="" />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default ExtraPart;
